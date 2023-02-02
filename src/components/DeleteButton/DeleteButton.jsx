bfeeddbd82f28bce6c2103178c5582b9bcb3ad3f
Natalie McMann <nmcmann110@gmail.com>
2023-02-02 10:34:48 -0600

    shelf.router.delete route is functional

---
+++ src/components/DeleteButton/DeleteButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+
+function DeleteButton() {
+    //should dispatch to saga http request to delete and item 
+    //with specific user and user-id
+    return(
+
+    )
+}
+
+export default DeleteButton;
