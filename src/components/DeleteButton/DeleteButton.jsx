8c026e49769b1dcd1bb553be68e798e6ab23c8ff
Natalie McMann <nmcmann110@gmail.com>
2023-02-02 11:11:11 -0600

    added dispatch to deleteItem function from DeleteButton component

--- src/components/DeleteButton/DeleteButton.jsx
+++ src/components/DeleteButton/DeleteButton.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 
-function DeleteButton() {
-    //should dispatch to saga http request to delete and item 
-    //with specific user and user-id
-    return(
+function DeleteButton({ item }) {
+    //declare dispatch
+    const dispatch = useDispatch();
 
+    //click should dispatch to saga http request to delete item by id
+    const handleClick = (event) => {
+        event.preventDefault();
+        const itemID = item.id;
+        dispatch({
+            type: 'SAGA/DELETE_ITEM',
+            payload: itemID
+        })
+    }
+
+    return(
+        <>
+            <button onClick={(event) => {handleClick(event)}}>Delete</button>
+        </>
     )
 }
 
